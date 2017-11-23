efec0877715f78d04d55ca6365a137030b7d8646
jonahs90 <jonathansanchez595@gmail.com>
2017-11-23 19:46:47 -0300

    Modificando el settings para usar SQLite en vez de MySQL

--- muni/muni/settings.py
+++ muni/muni/settings.py
@@ -77,13 +77,10 @@ WSGI_APPLICATION = 'muni.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'muni',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
