de3cc85788c04559d1737462c49d4a7618e1ea2e
jonahs90 <jonathansanchez595@gmail.com>
2017-11-23 13:49:24 -0300

    Mi sitio web Django, primer commit

---
+++ muni/blog/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render , get_object_or_404
+from django.utils import timezone
+from .models import Publicacion
+from .forms import PublicacionForm
+from django.shortcuts import redirect
+
+# Create your views here.
+
+def lista_publicaciones(request):
+    publicaciones = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/lista_publicaciones.html', {'publicaciones':publicaciones})
+
+def detalle_publicacion(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/detalle_publicacion.html', {'publicacion': publicacion})
+
+def nueva_publicacion(request):
+    if request.method == "POST":
+        form = PublicacionForm(request.POST)
+        if form.is_valid():
+            publicacion = form.save(commit=False)
+            publicacion.autor = request.user
+            publicacion.fecha_publicacion = timezone.now()
+            publicacion.save()
+            return redirect('detalle_publicacion', pk=publicacion.pk)
+    else:
+        form = PublicacionForm()
+    return render(request, 'blog/editar_publicacion.html', {'form': form})
+
+def editar_publicacion(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        form = PublicacionForm(request.POST, instance=publicacion)
+        if form.is_valid():
+            publicacion = form.save(commit=False)
+            publicacion.autor = request.user
+            publicacion.save()
+            return redirect('detalle_publicacion', pk=publicacion.pk)
+    else:
+        form = PublicacionForm(instance=publicacion)
+    return render(request, 'blog/editar_publicacion.html', {'form': form})
